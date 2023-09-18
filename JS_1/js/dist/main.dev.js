1546428ed1686568f57d6e71058373eb924c8e5d
Ihor <solodovnik.ihor21@gmail.com>
2023-09-19 02:16:44 +0300

    add_js

--- JS_1/js/dist/main.dev.js
+++ JS_1/js/dist/main.dev.js
@@ -1,13 +1,63 @@
 "use strict";
 
 function userName() {
-  var heroName = prompt("Add user name");
+  var username = prompt("Add your name");
 
-  if (heroName === null) {
-    console.log("Try again, please");
+  if (username && username.length) {
+    console.log("Hello, ".concat(username));
   }
 
-  if (heroName && heroName.length >= 2) {
-    console.log("Hello, ".concat(heroName));
+  if (username.length <= 3) {
+    console.log("Your name is too short");
+  }
+}
+
+function userAge() {
+  var userBirthday = parseInt(prompt("Add your birthday here"));
+  var currentYear = 2023;
+
+  if (!isNaN(userBirthday)) {
+    if (currentYear < userBirthday) {
+      console.log("You are not borned yet");
+    } else {
+      var _userAge = currentYear - userBirthday;
+
+      console.log(_userAge);
+    }
+  } else {
+    console.log("Add valid year");
+    userAge();
+  }
+}
+
+function squarePerimeter() {
+  var squarePerimeterLength = parseInt(prompt("Write length of square"));
+  var perimeterLength = squarePerimeterLength * 4;
+
+  if (!isNaN(squarePerimeterLength)) {
+    if (squarePerimeterLength <= 0) {
+      console.log("Try again");
+    } else {
+      console.log(perimeterLength);
+    }
+  } else {
+    console.log("Write a number");
+    squarePerimeter();
+  }
+}
+
+function circleArea() {
+  var circleRadius = parseInt(prompt("Write a circle radius"));
+  var circleAreais = Math.PI * Math.pow(circleRadius, 2);
+
+  if (!isNaN(circleRadius)) {
+    if (circleRadius <= 0) {
+      console.log("Only numbers");
+    } else {
+      console.log("Circle Area is ".concat(circleAreais));
+    }
+  } else {
+    console.log("Only numbers");
+    circleArea();
   }
 }
--- JS_1/js/main.js
+++ JS_1/js/main.js
@@ -1,10 +1,65 @@
 function userName() {
-    const heroName = prompt(`Add user name`);
+    const username = prompt(`Add your name`);
 
-    if (heroName === null) {
-        console.log(`Try again, please`);
+    if (username && username.length) {
+        console.log(`Hello, ${username}`);
     }
-    if (heroName && heroName.length >= 2) {
-        console.log(`Hello, ${heroName}`);
+    if (username.length <= 3) {
+        console.log(`Your name is too short`)
+    }
+}
+
+function userAge() {
+    const userBirthday = parseInt (prompt (`Add your birthday here`));
+
+    const currentYear = 2023;
+
+    if (!isNaN(userBirthday)){
+
+        if (currentYear < userBirthday) {
+            console.log(`You are not borned yet`);
+        } else {
+            const userAge = currentYear - userBirthday;
+            console.log (userAge);
+        }
+    } else {
+        console.log(`Add valid year`);
+        userAge()
+    } 
+}
+
+function squarePerimeter() {
+    const squarePerimeterLength = parseInt (prompt (`Write length of square`));
+
+    const perimeterLength = squarePerimeterLength * 4;
+
+    if (!isNaN(squarePerimeterLength)) {
+    
+        if (squarePerimeterLength <= 0) {
+            console.log(`Try again`);
+        } else {
+            console.log(perimeterLength); 
+        }
+    } else {
+        console.log(`Write a number`);
+        squarePerimeter();
+    }
+}
+
+function circleArea() {
+
+    const circleRadius = parseInt(prompt(`Write a circle radius`));
+
+    const circleAreais = (Math.PI * Math.pow(circleRadius, 2));
+
+    if (!isNaN(circleRadius)) {
+        if (circleRadius <= 0) {
+            console.log(`Only numbers`);
+        } else {
+            console.log(`Circle Area is ${circleAreais}`);
+        }
+    } else {
+        console.log(`Only numbers`);
+        circleArea()
     }
 }
